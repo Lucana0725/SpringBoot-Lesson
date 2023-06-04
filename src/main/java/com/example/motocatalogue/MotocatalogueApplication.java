1731fa9ff2d6987344e662e364fdeb6a40407ef0
Lucana <k.ark.scarlet72@gmail.com>
2023-06-04 15:16:51 +0900

    [FirstCommit]SpringBootを使用したCRUDアプリの作成学習。Controllerの作成とURLパラメータを用いたViewへのデータの表示

---
+++ src/main/java/com/example/motocatalogue/MotocatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.example.motocatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/motocatalogue/MotocatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motocatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
