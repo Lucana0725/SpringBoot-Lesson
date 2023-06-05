5a602ab468f409bfcc167ea6b18b61272ddb7bc7
Lucana <k.ark.scarlet72@gmail.com>
2023-06-05 18:50:12 +0900

    [Finish]MotosController.javaのmotos()において、Brandクラスのインスタンスを作成する際に'brandId:', 'brandName:'と記述していたことでビルドエラーになっていたので、解消。

--- src/main/java/com/example/motocatalogue/controllers/MotosController.java
+++ src/main/java/com/example/motocatalogue/controllers/MotosController.java
@@ -1,10 +1,16 @@
 package com.example.motocatalogue.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.motocatalogue.bean.Brand;
+import com.example.motocatalogue.bean.Motorcycle;
 
 @Controller
 public class MotosController {
@@ -17,6 +23,16 @@ public class MotosController {
 
   @GetMapping("/motos")
   public String motos() {
+    // ブランド
+    List<Brand> brands = new ArrayList<>();
+    brands.add(new Brand("01", "Honda"));
+    brands.add(new Brand("02", "Kawasaki"));
+
+    // バイク
+    List<Motorcycle> motos = new ArrayList<>();
+    motos.add(new Motorcycle(1, "GB350", 800, 1, "空冷", 500000, "いい音！", new Brand("01", "Honda"), 1, null, null));
+    motos.add(new Motorcycle(2, "Ninja", 800, 2, "水冷", 1000000, "かっこいい！", new Brand("02", "Kawasaki"), 1, null, null));
+
     return "moto_list";
   }
 }
